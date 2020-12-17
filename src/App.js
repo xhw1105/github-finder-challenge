cbf4683d6d02cf4b1e4f90b3e715f6b85615441c
Sasha Wang <xhw1105@gmail.com>
2020-12-17 23:41:05 +1300

    added the first hard coded user item with the right css.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import UserItems from "./components/users/UserItems";
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
+      <UserItems />
     </div>
   );
 }
