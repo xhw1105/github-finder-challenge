f27a1c4f565b7d64201cfb1360e9cd44736cd66d
Sasha Wang <xhw1105@gmail.com>
2020-12-17 23:25:11 +1300

    Added propTypes to Navbar.

--- src/layout/components/Navbar.js
+++ src/layout/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Navbar = (props) => {
   return (
@@ -13,6 +14,11 @@ const Navbar = (props) => {
 Navbar.defaultProps = {
   title: "Github Finder",
   icon: "fab fa-github",
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
 };
 
 export default Navbar;
