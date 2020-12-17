fa4d175b2df19951cbafffe37b0466646146f0aa
Sasha Wang <xhw1105@gmail.com>
2020-12-17 23:22:45 +1300

    A more completed Navbar with icon added.

--- src/layout/components/Navbar.js
+++ src/layout/components/Navbar.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-const Navbar = () => {
+const Navbar = (props) => {
   return (
     <nav className='navbar bg-primary '>
-      <h1>Github Finder</h1>
+      <h1>
+        <i className={props.icon} /> {props.title}
+      </h1>
     </nav>
   );
+};
+
+Navbar.defaultProps = {
+  title: "Github Finder",
+  icon: "fab fa-github",
 };
 
 export default Navbar;
