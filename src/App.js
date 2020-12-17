d978cfff6b6a38a3647c67e1ba4087e0510a65c1
Sasha Wang <xhw1105@gmail.com>
2020-12-17 23:14:12 +1300

    Added Navbar.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Navbar from "./layout/components/Navbar";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/layout/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className='navbar bg-primary '>
+      <h1>Github Finder</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
